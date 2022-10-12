66336f70880b28c25c2e51dd24447c6a3f4fe865
zsoon96 <pres0616@gmail.com>
2022-10-13 07:40:34 +0900

    admin 페이지 생성 및 GNB 추가

--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -10,6 +10,8 @@ export default function Gnb() {
         activeItem = "home";
     } else if (router.pathname === "/about") {
         activeItem = "about";
+    } else if (router.pathname === "/admin") {
+        activeItem = "admin";
     }
 
     function goLink (e, data) {
@@ -17,6 +19,8 @@ export default function Gnb() {
             router.push('/')
         } else if ( data.name === "about") {
             router.push('/about')
+        } else if ( data.name === "admin") {
+            router.push("/admin")
         }
     }
 
@@ -30,6 +34,11 @@ export default function Gnb() {
             <Menu.Item
                 name='about'
                 active={activeItem === 'about'}
+                onClick={goLink}
+            />
+            <Menu.Item
+                name='admin'
+                active={activeItem === 'admin'}
                 onClick={goLink}
             />
         </Menu>
