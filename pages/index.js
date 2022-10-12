04cd2f123cb604c671c2172c88cd249e8eb8fefa
zsoon96 <pres0616@gmail.com>
2022-10-13 07:01:57 +0900

    main/detail 페이지 - 로딩 처리

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from "next/head";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import ItemList from "../src/component/ItemList";
-import {Divider, Header} from "semantic-ui-react";
+import {Divider, Header, Loader} from "semantic-ui-react";
 
 export default function Home() {
 
     const [list, setList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
@@ -14,6 +15,7 @@ export default function Home() {
         axios.get(API_URL).then((res) => {
             console.log(res);
             setList(res.data)
+            setIsLoading(false)
         })
     }, []);
 
@@ -23,22 +25,23 @@ export default function Home() {
                 <title>HOMEㅣNext JS 기초</title>
             </Head>
 
-            <Header as="h3" style={{ paddingTop: 30 }}> 베스트 상품 </Header>
-            <Divider />
-            <ItemList list= {list.slice(0, 9)} />
+            {isLoading && (
+                <div style={{padding: "300px 0"}}>
+                    <Loader inline="centered" active/>
+                </div>
+            )}
 
-            <Header as="h3" style={{ paddingTop: 30 }}> 신상품 </Header>
-            <Divider />
-            <ItemList list= {list.slice(9)} />
-            {/*create-next-app으로 설치하면*/}
-            {/*<br />*/}
-            {/*1. 컴파일과 번들링이 자동으로 된다.(webpack과 babel)*/}
-            {/*<br />*/}
-            {/*2. 자동 리프레쉬 기능으로 수정하면 화면에 바로 반영된다.*/}
-            {/*<br />*/}
-            {/*3. 서버사이드 렌더링이 지원된다.*/}
-            {/*<br />*/}
-            {/*4. static 파일이 지원된다.*/}
+            {!isLoading && (
+                <>
+                    <Header as="h3" style={{paddingTop: 30}}> 베스트 상품 </Header>
+                    <Divider/>
+                    <ItemList list={list.slice(0, 9)}/>
+
+                    <Header as="h3" style={{paddingTop: 30}}> 신상품 </Header>
+                    <Divider/>
+                    <ItemList list={list.slice(9)}/>
+                </>
+            )}
         </div>
     )
 }
--- pages/view/[id].js
+++ pages/view/[id].js
@@ -2,22 +2,35 @@ import {useRouter} from "next/router";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import Item from '../../src/component/Item'
+import {Loader} from "semantic-ui-react";
 
 const Post = () => {
     const router = useRouter();
     const {id} = router.query;
 
     const [item, setItem] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
 
     useEffect(() => {
         axios.get(API_URL).then((res) => {
             setItem(res.data)
+            setIsLoading(false)
         })
     }, [])
 
-    return <Item item={item} />;
+    return (
+        <>
+            {isLoading ? (
+                <div style={{ padding: "300px 0" }}>
+                    <Loader inline="centered" active />
+                </div>
+            ) : (
+                <Item item={item} />
+            )}
+        </>
+    )
 }
 
 export default Post;
