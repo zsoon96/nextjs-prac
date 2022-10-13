38f202f63395759a130639efed5a30978dc514fe
zsoon96 <pres0616@gmail.com>
2022-10-13 21:18:24 +0900

    admin 페이지 - 접근 시 isLogin 확인

--- pages/admin.js
+++ pages/admin.js
@@ -1,4 +1,25 @@
+import axios from "axios";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
+
 export default function Admin() {
+
+    const router = useRouter();
+
+    function checkLogin() {
+        axios.get('/api/isLogin').then((res) => {
+            if (res.status === 200 && res.data.name) {
+                // 로그인
+            } else {
+                // 로그인 안됨
+                router.push('/login');
+            }
+        })
+    }
+
+    useEffect(()=> {
+        checkLogin();
+    }, [])
     return (
         <div>
             Admin Page
---
+++ pages/login.js
@@ -0,0 +1,22 @@
+import Axios from "axios";
+import { useRouter } from "next/router";
+import { Button, Form } from "semantic-ui-react";
+
+export default function Login() {
+
+    return (
+        <div style={{ padding: "100px 0", textAlign: "center" }}>
+            <Form>
+                <Form.Field inline>
+                    <input placeholder="ID" />
+                </Form.Field>
+                <Form.Field inline>
+                    <input type="password" placeholder="Password" />
+                </Form.Field>
+                <Button color="blue">
+                    Login
+                </Button>
+            </Form>
+        </div>
+    );
+}
